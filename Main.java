0f589ac69aca1abedef0f31e8b0c6f7785addad5
Student <mpodzielny18@gmail.com>
2024-10-12 18:19:57 +0200

    notatki

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main (String[] args) {
         //System.out.println("Hello World");
@@ -16,20 +18,36 @@ public class Main {
         //System.out.println(boolvalue1 || boolValue2);
 
 
-        int imputnumber = 5;
-        int testnumber = 7;
+       // int imputnumber = 5;
+        //int testnumber = 7;
 
-        boolean result = imputnumber > testnumber;
+        //boolean result = imputnumber > testnumber;
 
 
-        if (imputnumber > testnumber) {
-            System.out.println("Liczba " + imputnumber + " jest większa od liczby " + testnumber);
-        }
-        else if (imputnumber == testnumber) {
-            System.out.println("Liczby są równe");
-        }
-        else{
-            System.out.println("Liczba " + imputnumber + " jest mniejsza niż " + testnumber);
-        }
+        //if (imputnumber > testnumber) {
+          //  System.out.println("Liczba " + imputnumber + " jest większa od liczby " + testnumber);
+        //}
+        //else if (imputnumber == testnumber) {
+          //  System.out.println("Liczby są równe");
+        //}
+        //else{
+          //  System.out.println("Liczba " + imputnumber + " jest mniejsza niż " + testnumber);
+        //}
+        //for (int i = 0; i < 17; i++) {
+        //    System.out.print(i + ", ");
+        //}
+//        int i = 0;
+//        while (i < 9){
+//            System.out.println(i +" ");
+//            ++i;
+//        }
+//        System.out.println("Licznik pętli " + i);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę: ");
+        int imputnumber = scanner.nextInt();
+
+        System.out.println("Podana liczba to: " + imputnumber);
     }
+
 }
