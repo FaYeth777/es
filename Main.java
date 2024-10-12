476aa0f49f22462fa80dbc5cf7269d52c180c714
Student <mpodzielny18@gmail.com>
2024-10-12 17:41:13 +0200

    dodanie else

--- Main.java
+++ Main.java
@@ -2,18 +2,34 @@ public class Main {
     public static void main (String[] args) {
         //System.out.println("Hello World");
 
-        int firstnumber = 5;
-        float secondnumber = 8;
-        double result = firstnumber - secondnumber;
+        //int firstnumber = 5;
+        //float secondnumber = 8;
+        //double result = firstnumber - secondnumber;
 
         //System.out.println(result);
 
 
-        boolean boolvalue1 = true;
-        boolean boolValue2 = false;
+       // boolean boolvalue1 = true;
+       // boolean boolValue2 = false;
 
 
-        System.out.println(boolvalue1 || boolValue2);
+        //System.out.println(boolvalue1 || boolValue2);
 
+
+        int imputnumber = 5;
+        int testnumber = 7;
+
+        boolean result = imputnumber > testnumber;
+
+
+        if (imputnumber > testnumber) {
+            System.out.println("Liczba " + imputnumber + " jest większa od liczby " + testnumber);
+        }
+        else if (imputnumber == testnumber) {
+            System.out.println("Liczby są równe");
+        }
+        else{
+            System.out.println("Liczba " + imputnumber + " jest mniejsza niż " + testnumber);
+        }
     }
 }
