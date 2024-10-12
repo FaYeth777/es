171eb021b25617e7b4c908b47722eb8c99005ed5
Student <mpodzielny18@gmail.com>
2024-10-12 17:05:17 +0200

    xd

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main (String[] args) {
-        System.out.println("Hello World");
+        //System.out.println("Hello World");
+
+        int firstnumber = 5;
+        float secondnumber = 8;
+        double result = firstnumber - secondnumber;
+
+        //System.out.println(result);
+
+
+        boolean boolvalue1 = true;
+        boolean boolValue2 = false;
+
+
+        System.out.println(boolvalue1 || boolValue2);
+
     }
 }
